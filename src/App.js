c17a01b712e9fa5e696c0500f52c5f6f938260e4
David <wtih123@gmail.com>
2021-02-13 12:27:52 -0300

    Anotações, Primeiro component no react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from './Components/Header'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Header/>
+    </>
+  )
+}
+
+export default App;
